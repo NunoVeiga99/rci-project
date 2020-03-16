c1d69af82acdd0a6c9c4f51b5292265974742818
supercar1 <pedro.ramos.arade@hotmail.com>
2020-03-16 17:49:36 +0000

    seg fault; sentry com strcpy

--- main.c
+++ main.c
@@ -16,18 +16,33 @@
 #define smallchar 5
 #define bigchar 100
 
-    //Struct para guardar informações do servidor
-    struct server
+//Struct para guardar informações do servidor
+struct server
+{
+    int key;
+    char ipe[bigchar]; //read as IPê
+    char porto[bigchar];
+    struct server *next;
+    struct server *next2;
+};
+
+
+int countspace(char *s, char c)
+{
+    int i, count = 0;
+
+    for (i = 0; s[i]; i++)
     {
-        int key;
-        char ipe[bigchar];               //read as IPê
-        char porto[bigchar];
-        struct server *next;
-        struct server *next2;
-    } ;
+        if (s[i] == c)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
 
-int main(int argc, char*argv[])
+int main(int argc, char *argv[])
 {
     int fd, newfd, afd = 0;
     fd_set rfds;
@@ -38,7 +53,7 @@ int main(int argc, char*argv[])
     } state;
 
     int maxfd, counter;
-    
+
     //Inicializa o servidor
     struct server *servidor;
     servidor->next = NULL;
@@ -46,6 +61,7 @@ int main(int argc, char*argv[])
 
     const char s[2] = " "; //para procurar por espaços, usado no menu
     char *token;           //para guardar numa string o resto da string que se está a usar (menu)
+    char c = ' ';
 
     // Variáveis do servidor TCP
     struct addrinfo hints, *res;
@@ -55,9 +71,10 @@ int main(int argc, char*argv[])
     socklen_t addrlen;
     char *ptr, buffer[128];
     char comando[128]; //guarda o comando inserido pelo utilizador
+    char comandofull[128]; //guarda o comando inserido pelo utilizador e NÃO O ALTERA
     int i = 0;
-    int key=0;    
-
+    int key = 0;
+    int count = 0; //conta o num. de espaços no menu (entry e sentry)
 
     // Variáveis do servidor udp
     struct addrinfo udphints, *udpres;
@@ -65,18 +82,18 @@ int main(int argc, char*argv[])
     ssize_t nread;
 
     //Se na chamada do programa
-    if(argc != 3){
+    if (argc != 3)
+    {
         printf("ERRO.\nInicialização inválida, volte a correr o programa!\n");
         exit(0);
     }
 
     //Guarda o ip e o porto na estrutura
-    strcpy(servidor->ipe, argv[1]); 
+    strcpy(servidor->ipe, argv[1]);
     printf("IP: %s\n", servidor->ipe);
     //servidor->porto = atoi(argv[2]);
-    strcpy(servidor->porto, argv[2]); 
+    strcpy(servidor->porto, argv[2]);
     printf("Porto: %s\n", servidor->porto);
-
 
     // Cria socket TCP
     if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
@@ -87,7 +104,7 @@ int main(int argc, char*argv[])
     hints.ai_flags = AI_PASSIVE;
 
     //Obtém o o endereço e atribui um porto ao servidor TCP
-    if ((errcode = getaddrinfo(servidor->ipe, servidor->porto , &hints, &res)) != 0) /*error*/
+    if ((errcode = getaddrinfo(servidor->ipe, servidor->porto, &hints, &res)) != 0) /*error*/
         exit(1);
 
     //bind e listen TCP
@@ -143,10 +160,12 @@ int main(int argc, char*argv[])
         if (FD_ISSET(0, &rfds))
         {
 
-            //printf("Insira um comando:\n");  
-            //fflush(stdout);          
+            //printf("Insira um comando:\n");
+            //fflush(stdout);
             //scanf("%s", comando);
             fgets(comando, 128, stdin); //receber input do teclado
+
+            strcpy(comandofull, comando);
 
             token = strtok(comando, s); //procurar no input onde está o espaço
             printf("comando: %s\n", comando);
@@ -158,18 +177,22 @@ int main(int argc, char*argv[])
                 token = strtok(NULL, s);
                 printf("token %d: %s\n", i, token);
 
-                servidor->key  = atoi(token);
+                servidor->key = atoi(token);
                 printf("A chave escolhida é: %d\n", servidor->key);
-                
             }
             else if (strcmp(comando, "entry") == 0)
             {
                 printf("Escolheu: entry\n");
-
             }
             else if (strcmp(comando, "sentry") == 0)
             {
                 printf("Escolheu: sentry\n");
+
+                count = countspace(comandofull, c);
+                printf("token: %s \n", comandofull);
+                printf("character '%c' occurs %d times \n ", c, count);
+
+
                 /*
                 while(token!=NULL)
                 {
@@ -177,10 +200,8 @@ int main(int argc, char*argv[])
                     
                 }
                 */
-            //strcpy(servidor->next->ipe,argv[2]);
-            //printf("Succ ip: %s", servidor->next->ipe);
-
-
+                //strcpy(servidor->next->ipe,argv[2]);
+                //printf("Succ ip: %s", servidor->next->ipe);
             }
             else if (strcmp(comando, "leave\n") == 0)
             {
@@ -274,19 +295,4 @@ int main(int argc, char*argv[])
         }
     } //while(1)
     /*close(fd);exit(0);*/
-}
-
-
-int countspace(char *s,char c)
-{
-    int i,count=0;
-
-    for(i=0;s[i];i++)  
-    {
-    	if(s[i]==c)
-    	{
-          count++;
-		}
- 	}
- 	return count;    		  
 }
